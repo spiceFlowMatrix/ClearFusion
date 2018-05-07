939bd32384c9219b4533d95a5e2f58973b54076d
alpitg <alpit.gajbhiye@smartdatainc.net>
2018-05-07 19:44:44 +0530

    COMMENT

--- DataAccess/Migrations/20180507123833_InterviewTechnicalQuestions.Designer.cs
+++ DataAccess/Migrations/20180507123833_InterviewTechnicalQuestions.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180507123833_InterviewTechnicalQuestions")]
+    partial class InterviewTechnicalQuestions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20180507123833_InterviewTechnicalQuestions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Migrations
+{
+    public partial class InterviewTechnicalQuestions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InterviewTechnicalQuestion_TechnicalQuestion_TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InterviewTechnicalQuestion_TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion");
+
+            migrationBuilder.DropColumn(
+                name: "TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Question",
+                table: "InterviewTechnicalQuestion",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Question",
+                table: "InterviewTechnicalQuestion");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterviewTechnicalQuestion_TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion",
+                column: "TechnicalQuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InterviewTechnicalQuestion_TechnicalQuestion_TechnicalQuestionId",
+                table: "InterviewTechnicalQuestion",
+                column: "TechnicalQuestionId",
+                principalTable: "TechnicalQuestion",
+                principalColumn: "TechnicalQuestionId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
