9c2c72a8b7f7b89099364617f0227c77fa51a591
shubhamk <shubhamk@SDD-65>
2018-05-18 21:16:38 +0530

    commits

---
+++ DataAccess/Migrations/20180518153804_InterviewMembersTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class InterviewMembersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeAppraisalTeamMember",
+                columns: table => new
+                {
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    CreatedById = table.Column<string>(nullable: true),
+                    ModifiedById = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    EmployeeAppraisalTeamMemberId = table.Column<int>(type: "serial", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    EmployeeAppraisalDetailsId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeAppraisalTeamMember", x => x.EmployeeAppraisalTeamMemberId);
+                    table.ForeignKey(
+                        name: "FK_EmployeeAppraisalTeamMember_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EmployeeAppraisalTeamMember_AspNetUsers_ModifiedById",
+                        column: x => x.ModifiedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeAppraisalTeamMember_CreatedById",
+                table: "EmployeeAppraisalTeamMember",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeAppraisalTeamMember_ModifiedById",
+                table: "EmployeeAppraisalTeamMember",
+                column: "ModifiedById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeAppraisalTeamMember");
+        }
+    }
+}
