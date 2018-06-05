2645daf626a031a55cc1abd563c4542de7250c29
alpitg <alpit.gajbhiye@smartdatainc.net>
2018-06-05 09:35:10 +0530

    migrataion and cache

---
+++ DataAccess/Migrations/20180604131701_StoreInChang3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class StoreInChang3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreInventories_ChartAccountDetail_InventoryAccount",
+                table: "StoreInventories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventoryAccount",
+                table: "StoreInventories",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreInventories_ChartAccountDetail_InventoryAccount",
+                table: "StoreInventories",
+                column: "InventoryAccount",
+                principalTable: "ChartAccountDetail",
+                principalColumn: "AccountCode",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreInventories_ChartAccountDetail_InventoryAccount",
+                table: "StoreInventories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventoryAccount",
+                table: "StoreInventories",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreInventories_ChartAccountDetail_InventoryAccount",
+                table: "StoreInventories",
+                column: "InventoryAccount",
+                principalTable: "ChartAccountDetail",
+                principalColumn: "AccountCode",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
