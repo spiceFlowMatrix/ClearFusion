3d62f1bde6bd4de0afad9bf7c09c6c16255d6e14
alpitg <alpit.gajbhiye@smartdatainc.net>
2018-06-09 11:50:59 +0530

    minor

--- HumanitarianAssistance.Entities/Models/Store/ItemOrderModel.cs
+++ HumanitarianAssistance.Entities/Models/Store/ItemOrderModel.cs
@@ -6,13 +6,18 @@ namespace HumanitarianAssistance.ViewModels.Models.Store
 {
     public class ItemOrderModel
     {
-		public string OrderId { get; set; }
-		public string Purchase { get; set; }								// PurchaseID DropDown
-		public string InventoryItem { get; set; }							// InventoryID DropDown
-		public int IssuedQuantity { get; set; }								// Text Box
-		public bool MustReturn { get; set; }								// CheckBox
-		public int IssuedToEmployeeId { get; set; }							// EmployeeID DropDown
-		public DateTime IssueDate { get; set; }								
-		public DateTime ReturnedDate { get; set; }
-	}
+        public string OrderId { get; set; }
+        public string Purchase { get; set; }                                // PurchaseID DropDown
+        public string InventoryItem { get; set; }                           // InventoryID DropDown
+        public int IssuedQuantity { get; set; }                             // Text Box
+        public bool MustReturn { get; set; }                                // CheckBox
+        public int IssuedToEmployeeId { get; set; }                         // EmployeeID DropDown
+        public DateTime IssueDate { get; set; }
+        public DateTime ReturnedDate { get; set; }
+
+        //TODo: For get
+        //public string InventoryName { get; set; }
+        //public string InventoryItemName { get; set; }
+
+    }
 }
