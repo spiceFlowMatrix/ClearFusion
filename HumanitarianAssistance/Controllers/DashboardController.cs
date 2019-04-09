abb4b2b566f806a5c4b9f0935c1240abef9c9d22
ramansdd <ramanrawat@smartdatainc.net>
2019-04-09 11:23:28 +0530

    TrainingLink merge conflict resolved

--- HumanitarianAssistance/Controllers/DashboardController.cs
+++ HumanitarianAssistance/Controllers/DashboardController.cs
@@ -1,6 +1,6 @@
 
+using Microsoft.Extensions.Configuration;
 using DataAccess.DbEntities;
-using HumanitarianAssistance.Service.APIResponses;
 using HumanitarianAssistance.Service.interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -9,44 +9,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HumanitarianAssistance.WebAPI.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Dashboard/[Action]")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class DashboardController : Controller
+  [Produces("application/json")]
+  [Route("api/Dashboard/[Action]")]
+  [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+  public class DashboardController : Controller
+  {
+    private readonly UserManager<AppUser> _userManager;
+    private IDashboardService _iDashboard;
+    private IConfiguration _configuration;
+    public DashboardController(
+         UserManager<AppUser> userManager,
+         IDashboardService iDashboard,
+         IConfiguration configuration
+        )
     {
-        private readonly UserManager<AppUser> _userManager;
-        private IDashboardService _iDashboard;
-        private IConfiguration _configuration;
-        public DashboardController(
-       UserManager<AppUser> userManager,
-      IDashboardService iDashboard,
-     IConfiguration configuration
-            )
-        {
-            _configuration = configuration;
-            _userManager = userManager;
-            _iDashboard = iDashboard;
-        }
-
-        //[HttpGet]
-        //public APIResponse GetTrainingLink()
-        //{
-        //  APIResponse response = _iDashboard.GetTrainingLink();
-        //  return response;
-        //}
-
-        [HttpGet]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public IActionResult GetTrainingLink()
-        {
-            if (_configuration["DOCS_URL"] != null)
-            {
-                return Ok(_configuration["DOCS_URL"].ToString());
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
+      _configuration = configuration;
+      _userManager = userManager;
+      _iDashboard = iDashboard;
     }
+
+    //[HttpGet]
+    //public APIResponse GetTrainingLink()
+    //{
+    //  APIResponse response = _iDashboard.GetTrainingLink();
+    //  return response;
+    //}
+
+    [HttpGet]
+    public IActionResult GetTrainingLink()
+    {
+      if (_configuration["DOCS_URL"] != null)
+      {
+        return Ok(_configuration["DOCS_URL"].ToString());
+      }
+      else
+      {
+        return NotFound();
+      }
+    }
+  }
 }
