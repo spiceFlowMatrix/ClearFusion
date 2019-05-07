c3b2be16d2067a1d289e9f639d9c68d0ea572eb6
ramansdd <ramanrawat@smartdatainc.net>
2019-05-07 18:34:49 +0530

    filter changes for activity

--- HumanitarianAssistance.Entities/Models/Project/ProjectActivityModel.cs
+++ HumanitarianAssistance.Entities/Models/Project/ProjectActivityModel.cs
@@ -11,13 +11,6 @@ namespace HumanitarianAssistance.ViewModels.Models.Project
         {
             Recurring = false;
             RecurringCount = 0;
-            //ImplementationProgress = 0;
-            //ImplementationStatus = false;
-
-            //MonitoringProgress = 0;
-            //MonitoringStatus = false;
-            //MonitoringScore = 0;
-            //MonitoringFrequency = 0;
         }
         //Planning
         public long? ActivityId { get; set; }
@@ -44,37 +37,5 @@ namespace HumanitarianAssistance.ViewModels.Models.Project
 
         public long? ParentId { get; set; }
         public int? Target { get; set; }
-
-        ////Implementation
-        //[Range(0, 100)]
-        //public float? ImplementationProgress { get; set; }
-        //public bool? ImplementationStatus { get; set; }
-        //public DateTime? ActualStartDate { get; set; }
-        //public DateTime? ActualEndDate { get; set; }
-        //public string ImplementationMethod { get; set; }
-        //public string ImplementationChalanges { get; set; }
-        //public string OvercomingChallanges { get; set; }
-        //public DateTime? ExtensionStartDate { get; set; }
-        //public DateTime? ExtensionEndDate { get; set; }
-
-        ////Monitoring
-        //[Range(0, 100)]
-        //public float? MonitoringProgress { get; set; }
-        //public bool? MonitoringStatus { get; set; }
-
-        //public int? MonitoringScore { get; set; }
-
-        //public int? MonitoringFrequency { get; set; }
-        //public string VerificationSource { get; set; }
-
-        //public string Strengths { get; set; }
-
-        //public string Weeknesses { get; set; }
-        //public string MonitoringChallenges { get; set; }
-        //public string Recommendation { get; set; }
-        //public int? OfficeId { get; set; }
-
-        //public string OfficeName { get; set; }
-        //public string Comments { get; set; }
     }
 }
