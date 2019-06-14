22eab1d411de7c3bf6a94e53cb03c04c5991a463
ramanr <ramanr@SDD-38>
2019-06-14 16:07:38 +0530

    changes for employee attendance

---
+++ HumanitarianAssistance.Service/CommonUtility/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanitarianAssistance.Service.CommonUtility
+{
+    public static class ExtensionMethods
+    {
+        public static int RoundOff(this decimal i)
+        {
+            return ((int)Math.Round((i / 10.0m))) * 10;
+        }
+    }
+}
