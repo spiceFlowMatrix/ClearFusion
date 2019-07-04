6be7352b2ae6002fd18074776afc46175f478412
poonamk <poonamk@SDD-29>
2019-07-04 18:49:40 +0530

    proposal page changes

--- HumanitarianAssistance.WebApi/Controllers/ProjectController.cs
+++ HumanitarianAssistance.WebApi/Controllers/ProjectController.cs
@@ -853,7 +853,7 @@ namespace HumanitarianAssistance.WebAPI.Controllers
       {
         string logginUserEmailId = user.Email;
         var id = user.Id;
-        apiRespone = _iProject.AddEditProjectProposalDetail(model, id, logginUserEmailId);
+        apiRespone =await _iProject.AddEditProjectProposalDetail(model, id, logginUserEmailId);
       }
       return apiRespone;
     }
