ee1abd6c7a4c2c42fec39496387d57674c524d58
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-07-17 23:20:23 +0530

    UseExceptionHandler

--- HumanitarianAssistance.WebApi/Startup.cs
+++ HumanitarianAssistance.WebApi/Startup.cs
@@ -297,12 +297,12 @@ namespace HumanitarianAssistance.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            // else
-            // {
-            //     app.UseExceptionHandler("/Error");
-            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //     app.UseHsts();
-            // }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
