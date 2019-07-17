01a9ce7cd2eb51f81093ee195ba29d0593531778
ramanr <ramanrawat03@gmail.com>
2019-07-18 02:17:02 +0530

    change

--- HumanitarianAssistance.WebApi/Startup.cs
+++ HumanitarianAssistance.WebApi/Startup.cs
@@ -299,7 +299,8 @@ namespace HumanitarianAssistance.WebApi
                 app.UseDeveloperExceptionPage();
             }
             else
-            {
+            { 
+
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
