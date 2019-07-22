41cdbf0992495fe7c8461f4be7ac66edb0c68d79
ramanr <ramanrawat03@gmail.com>
2019-07-22 12:03:26 +0530

    minor class add

---
+++ HumanitarianAssistance.Service/Classes/ChatService.cs
@@ -0,0 +1,11 @@
+﻿using HumanitarianAssistance.Service.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanitarianAssistance.Service.Classes
+{
+    public class ChatService : IChat
+    {
+    }
+}
---
+++ HumanitarianAssistance.Service/interfaces/IChat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanitarianAssistance.Service.interfaces
+{
+    public interface IChat
+    {
+    }
+}
