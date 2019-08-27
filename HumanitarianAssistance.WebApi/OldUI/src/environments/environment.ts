1fc234959749dece52c3b9133c7881842ca73bbe
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-08-27 17:52:09 +0530

    environment changes

--- HumanitarianAssistance.WebApi/OldUI/src/environments/environment.ts
+++ HumanitarianAssistance.WebApi/OldUI/src/environments/environment.ts
@@ -5,7 +5,14 @@
 
 export const environment = {
   production: false,
-    apiUrl: 'http://localhost:5000/api/',
-    docUrl: 'http://localhost:5000/Docs/',
-    hubUrl: 'http://localhost:5000/chathub/'
+    apiUrl: '/api/',
+    docUrl: '/Docs/',
+    hubUrl: '/chathub/'
 };
+
+// export const environment = {
+//   production: false,
+//     apiUrl: 'http://localhost:5000/api/',
+//     docUrl: 'http://localhost:5000/Docs/',
+//     hubUrl: 'http://localhost:5000/chathub/'
+// };
--- HumanitarianAssistance.WebApi/Startup.cs
+++ HumanitarianAssistance.WebApi/Startup.cs
@@ -184,7 +184,7 @@ namespace HumanitarianAssistance.WebApi
                     if (env.IsDevelopment())
                     {
                         // it will use package.json & will search for start command to run
-                        //spa.UseAngularCliServer(npmScript: "start");
+                        spa.UseAngularCliServer(npmScript: "start");
                     }
                     else
                     {
@@ -210,7 +210,7 @@ namespace HumanitarianAssistance.WebApi
                 if (env.IsDevelopment())
                 {
                     // it will use package.json & will search for start command to run
-                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseAngularCliServer(npmScript: "start");
                 }
                 else
                 {
