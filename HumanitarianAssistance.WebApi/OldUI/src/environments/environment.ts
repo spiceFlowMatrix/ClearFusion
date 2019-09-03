41fa1acd3f854608f0ec99b49908e2471506a2b3
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-09-03 16:25:15 +0530

    enviroment changes

--- HumanitarianAssistance.WebApi/OldUI/src/environments/environment.ts
+++ HumanitarianAssistance.WebApi/OldUI/src/environments/environment.ts
@@ -1,18 +1,13 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+export const environment = {
+  production: false,
+    apiUrl: '/api/',
+    docUrl: '/Docs/',
+    hubUrl: '/chathub/'
+};
 
 // export const environment = {
 //   production: false,
-//     apiUrl: '/api/',
-//     docUrl: '/Docs/',
-//     hubUrl: '/chathub/'
+//     apiUrl: 'http://localhost:5000/api/',
+//     docUrl: 'http://localhost:5000/Docs/',
+//     hubUrl: 'http://localhost:5000/chathub/'
 // };
-
-export const environment = {
-  production: false,
-    apiUrl: 'http://localhost:5000/api/',
-    docUrl: 'http://localhost:5000/Docs/',
-    hubUrl: 'http://localhost:5000/chathub/'
-};
