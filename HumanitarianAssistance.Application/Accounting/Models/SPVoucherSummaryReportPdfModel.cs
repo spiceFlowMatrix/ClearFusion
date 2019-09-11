5b80b633f9e45f9d39ea7f423d1256d1b91cdd73
alpit-g <alpit.gajbhiye@smartdatainc.net>
2019-09-11 12:41:25 +0530

    voucherSummary

--- HumanitarianAssistance.Application/Accounting/Models/SPVoucherSummaryReportPdfModel.cs
+++ HumanitarianAssistance.Application/Accounting/Models/SPVoucherSummaryReportPdfModel.cs
@@ -15,15 +15,14 @@ namespace HumanitarianAssistance.Application.Accounting.Models
         public string VoucherDate { get; set; }
         public string Region { get; set; }
         public string Description { get; set; }
-        public IList<SPTransactionSummaryReportPdfModel> TransactionDetails { get; set; }
         public string TotalCredit { get; set; }
         public string TotalDebit { get; set; }
-    }
 
-    public class SPTransactionSummaryReportPdfModel
-    {
+        // public IList<SPTransactionSummaryReportPdfModel> TransactionDetails { get; set; }
+
+        
         public string AccountNo { get; set; }
-        public string Description { get; set; }
+        public string TransactionDescription { get; set; }
         public string Debit { get; set; }
         public string Credit { get; set; }
         public string BudgetLine { get; set; }
@@ -33,4 +32,18 @@ namespace HumanitarianAssistance.Application.Accounting.Models
         public string Project { get; set; }
         public string Job { get; set; }
     }
+
+    // public class SPTransactionSummaryReportPdfModel
+    // {
+    //     public string AccountNo { get; set; }
+    //     public string Description { get; set; }
+    //     public string Debit { get; set; }
+    //     public string Credit { get; set; }
+    //     public string BudgetLine { get; set; }
+    //     public string Area { get; set; }
+    //     public string Sector { get; set; }
+    //     public string Program { get; set; }
+    //     public string Project { get; set; }
+    //     public string Job { get; set; }
+    // }
 }
