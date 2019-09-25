f7e163dc4f221d7cabfdeb44d42d01c69082e348
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-09-25 10:30:22 +0530

    added pdf export for voucher summary

--- HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/accounting/voucher-summary-report/voucher-summary-report.component.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/accounting/voucher-summary-report/voucher-summary-report.component.ts
@@ -44,7 +44,7 @@ import { VoucherSummaryFilterComponent } from './voucher-summary-filter/voucher-
 export class VoucherSummaryReportComponent implements OnInit, OnDestroy {
 
   //#endregion
-
+  recordtypetext;
   constructor(
     private voucherSummaryService: VoucherSummaryReportService,
     private appUrl: AppUrlService,
@@ -386,6 +386,14 @@ export class VoucherSummaryReportComponent implements OnInit, OnDestroy {
   filterApplied(value: VoucherSummaryFilterModel) {
     this.onInitialize();
     this.filterData = value;
+    if (value.RecordType.toString() === '2'){
+      this.recordtypetext = 'Consolidated';
+    } else {
+      this.recordtypetext = 'Single';
+    }
+    // tslint:disable-next-line: no-unused-expression
+    this.recordtypetext = 'Note: Showing Vouchers for Record Type as ' + this.recordtypetext +
+    ' and Currency as ' + this.currencyList[this.currencyList.findIndex(x => x.CurrencyId === value.Currency)]['CurrencyCode'];
     this.getVoucherSummaryList(value);
   }
 
