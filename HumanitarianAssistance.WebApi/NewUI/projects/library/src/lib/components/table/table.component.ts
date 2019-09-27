275ea0b17442c7d19c64d267d1e6da68a659b8b7
ramanr <ramanrawat03@gmail.com>
2019-09-27 14:53:18 +0530

    changes for purchase list

--- HumanitarianAssistance.WebApi/NewUI/projects/library/src/lib/components/table/table.component.ts
+++ HumanitarianAssistance.WebApi/NewUI/projects/library/src/lib/components/table/table.component.ts
@@ -13,7 +13,7 @@ export class TableComponent implements OnInit, OnChanges {
   @Input() subHeaders: Observable<string[]>;
   @Input() items: Observable<Array<Object>>;
   @Input() subTitle: string;
-  subItemHeaders: Observable<string[]>
+  subItemHeaders: Observable<string[]>;
 
   subItems: Array<Object> = [];
   itemHeaders: Observable<string[]>;
@@ -22,19 +22,21 @@ export class TableComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit() {
+
   }
   ngOnChanges(): void {
     if (this.items) {
       this.items.subscribe(res => {
+        this.subItems = [];
         if (res == null || res.length > 0) {
           res.forEach((element, i) => {
 
             this.subItems.push(element['subItems']);
             delete element['subItems'];
           });
+          console.log(this.subItems);
           this.itemHeaders = of(Object.keys(res[0]));
           this.subItemHeaders = of(Object.keys(this.subItems[0][0]));
-
         }
 
       });
