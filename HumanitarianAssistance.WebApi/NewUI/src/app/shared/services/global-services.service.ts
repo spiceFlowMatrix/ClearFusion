07e6e6cbe2640fc3b6c784f7c9dba3b8f282b836
avinashr <avinashr@DESKTOP-6OB5DTP>
2019-09-30 16:43:40 +0530

    retrying for slow internet connection

--- HumanitarianAssistance.WebApi/NewUI/src/app/shared/services/global-services.service.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/shared/services/global-services.service.ts
@@ -5,6 +5,8 @@ import { GlobalErrorHandler } from './global-error-handler';
 import { CommonLoaderService } from '../common-loader/common-loader.service';
 import { finalize } from 'rxjs/internal/operators/finalize';
 import { Observable } from 'rxjs/internal/Observable';
+import { timeout, retryWhen, tap, delay, take } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +15,28 @@ export class GlobalService {
 
   errHandler: GlobalErrorHandler;
 
-  constructor(private http: HttpClient, private loader: CommonLoaderService) { }
+  constructor(private http: HttpClient, private loader: CommonLoaderService,public toastr: ToastrService) { }
 
   //#region  "POST"
   post(url: string, data, options?: any): Observable<any> {
     return this.http.post<any>(url, data, options).pipe(
       map((response) => response),
+      timeout(200000),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
-      })
+      }
+      )
 
       // catchError(this.errHandler.handleError('dfdf', []))
       // catchError((error) => { console.log(error); return ""; })
@@ -33,6 +48,17 @@ export class GlobalService {
   getList(url: string): Observable<any> {
     return this.http.get<any>(url).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
@@ -44,16 +70,38 @@ export class GlobalService {
   getListById(url: string, id: number): Observable<any> {
     return this.http.post<any>(url, id).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
     );
   }
 
-   //#region "GET_ITEM_BY_ID"
-   getItemById(url: string, Id: number): Observable<any> {
+  //#region "GET_ITEM_BY_ID"
+  getItemById(url: string, Id: number): Observable<any> {
     return this.http.get<any>(url + '?Id=' + Id).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
@@ -64,6 +112,17 @@ export class GlobalService {
   getDataById(url: string, options?: any): Observable<any> {
     return this.http.get<any>(url, options).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
@@ -74,6 +133,17 @@ export class GlobalService {
   getListByIdAndDate(url: string, data): Observable<any> {
     return this.http.post(url, data).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
@@ -84,6 +154,17 @@ export class GlobalService {
     // this.loader.showLoader();
     return this.http.post<any>(url, id).pipe(
       map((response) => response),
+      retryWhen(errors =>
+
+        errors.pipe(
+          tap(r => {
+            this.toastr.warning('Slow internet connection retrying ...')
+          }
+          ),
+          delay(10000),
+          take(3)
+        )
+      ),
       finalize(() => {
         // this.loader.hideLoader();
       })
