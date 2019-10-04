615a12dcfd587c0c4ad9b0c9d62c78f66616787e
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-10-04 12:00:39 +0530

    voucher summary report moved to voucher page

--- HumanitarianAssistance.Application/Accounting/Queries/GetAllVoucherSummaryReportPdfQuery.cs
+++ HumanitarianAssistance.Application/Accounting/Queries/GetAllVoucherSummaryReportPdfQuery.cs
@@ -6,13 +6,19 @@ namespace HumanitarianAssistance.Application.Accounting.Queries
 {
     public class GetAllVoucherSummaryReportPdfQuery : IRequest<byte[]>
     {
-        public List<long> Accounts { get; set; }
-        public List<long> BudgetLines { get; set; }
-        public int Currency { get; set; }
-        public List<int> Journals { get; set; }
-        public List<int> Offices { get; set; }
-        public List<long> ProjectJobs { get; set; }
-        public List<long> Projects { get; set; }
-        public int RecordType { get; set; }
+        // public List<long> Accounts { get; set; }
+        // public List<long> BudgetLines { get; set; }
+        // public int Currency { get; set; }
+        // public List<int> Journals { get; set; }
+        // public List<int> Offices { get; set; }
+        // public List<long> ProjectJobs { get; set; }
+        // public List<long> Projects { get; set; }
+        // public int RecordType { get; set; }
+        public string FilterValue { get; set; }
+        public bool VoucherNoFlag { get; set; }
+        public bool ReferenceNoFlag { get; set; }
+        public bool DescriptionFlag { get; set; }
+        public bool JournalNameFlag { get; set; }
+        public bool DateFlag { get; set; }
     }
 }
