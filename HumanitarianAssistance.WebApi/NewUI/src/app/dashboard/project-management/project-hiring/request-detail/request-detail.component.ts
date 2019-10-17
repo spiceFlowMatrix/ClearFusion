698827a8462f173d18a54db10fbaebbb19f20fbb
avinashr <avinashr@DESKTOP-6OB5DTP>
2019-10-17 12:02:36 +0530

    Request detail form added

--- HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/project-management/project-hiring/request-detail/request-detail.component.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/project-management/project-hiring/request-detail/request-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { HiringList } from '../models/hiring-list';
 
 @Component({
   selector: 'app-request-detail',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-detail.component.scss']
 })
 export class RequestDetailComponent implements OnInit {
-
+  newCandidatesHeaders$ = of(['Employee Code', 'Full Name', 'Gender', 'Interview', 'Candidate Status']);
+  newCandidatesList$: Observable<HiringList[]>;
   constructor() { }
 
   ngOnInit() {
+    this.newCandidatesList$ = of ([
+      {
+        JobCode:'E1534',
+        JobGrade: 'Employee Name',
+        FilledVacancies:'Male',
+        PayCurrency:'<a>Interview Id</a>',
+        PayRate:'<p style="color: #bcbc1e";>pending interview</p>',
+      }
+   
+      
+    ] as HiringList[])
   }
 
 }
