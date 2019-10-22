87590dfaa4928e8f8f2573d82fcfd0abb93edfb1
arjuns <arjuns@SDD-1>
2019-10-22 15:25:02 +0530

    migration updated

--- HumanitarianAssistance.Domain/Entities/Project/JobHiringDetail.cs
+++ HumanitarianAssistance.Domain/Entities/Project/JobHiringDetail.cs
@@ -1,37 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HumanitarianAssistance.Domain.Entities.Project
-{
-    public class JobHiringDetail : BaseEntity
-    {
+namespace HumanitarianAssistance.Domain.Entities.Project {
+    public class JobHiringDetail : BaseEntity {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column(Order = 1)]
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+        [Column (Order = 1)]
         public long JobId { get; set; }
 
-        [StringLength(50)]
+        [StringLength (50)]
         public string JobCode { get; set; }
         public string JobDescription { get; set; }
         public int? ProfessionId { get; set; }
 
-        [ForeignKey("ProfessionId")]
-        public ProfessionDetails ProfessionDetails { get; set; }  
-        public int? TotalVacancies { get; set; }      
-        public double? PayRate { get; set; }        
+        [ForeignKey ("ProfessionId")]
+        public ProfessionDetails ProfessionDetails { get; set; }
+        public int? TotalVacancies { get; set; }
+        public double? PayRate { get; set; }
         public int OfficeId { get; set; }
 
-        [ForeignKey("OfficeId")]
+        [ForeignKey ("OfficeId")]
         public OfficeDetail OfficeDetails { get; set; }
         public int? GradeId { get; set; }
 
-        [ForeignKey("GradeId")]
+        [ForeignKey ("GradeId")]
         public JobGrade JobGrade { get; set; }
 
         public int? CurrencyId { get; set; }
 
-        [ForeignKey("CurrencyId")]
+        [ForeignKey ("CurrencyId")]
 
         public CurrencyDetails CurrencyDetail { get; set; }
+        public long ProjectId { get; set; }
+
+        [ForeignKey ("ProjectId")]
+        public virtual ProjectDetail ProjectDetail { get; set; }
     }
 }
