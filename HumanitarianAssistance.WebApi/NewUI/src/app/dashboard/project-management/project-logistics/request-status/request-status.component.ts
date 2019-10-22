1f002e8032e1d811c2586cb4031736198e5da06f
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-10-22 12:44:46 +0530

    logistic component updated

--- HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/project-management/project-logistics/request-status/request-status.component.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/project-management/project-logistics/request-status/request-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-request-status',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestStatusComponent implements OnInit {
   selected_case = 3;
+  purchasedItemsHeaders$ = of(['Item', 'Quantity', 'Final Cost']);
+  purchasedItemsData$ = of([{'Item': 'Item1', 'Quantity': '12', 'Final Cost': '2500'}]);
   constructor() { }
 
   ngOnInit() {
