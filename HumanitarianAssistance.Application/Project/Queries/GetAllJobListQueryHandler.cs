d211da0c22b1ab569180757f20eff0d5e18a06bf
arjuns <arjuns@SDD-1>
2019-10-23 12:26:13 +0530

    minor chnages

--- HumanitarianAssistance.Application/Project/Queries/GetAllJobListQueryHandler.cs
+++ HumanitarianAssistance.Application/Project/Queries/GetAllJobListQueryHandler.cs
@@ -19,7 +19,7 @@ namespace HumanitarianAssistance.Application.Project.Queries {
         public async Task<ApiResponse> Handle (GetAllJobListQuery request, CancellationToken cancellationToken) {
             ApiResponse response = new ApiResponse ();
             try {
-                var jobDetailList = await _dbContext.JobHiringDetail.Where (x => x.IsDeleted == false && x.ProjectId == request.ProjectId && x.ProfessionId == request.ProfessionId && x.OfficeId == request.OfficeId).Select (x => new JobHiringDetailModel {
+                var jobDetailList = await _dbContext.ProjectJobHiringDetail.Where (x => x.IsDeleted == false && x.ProjectId == request.ProjectId && x.ProfessionId == request.ProfessionId && x.OfficeId == request.OfficeId).Select (x => new JobHiringDetailModel {
                     JobId = x.JobId,
                         JobCode = x.JobCode
                 }).ToListAsync ();
