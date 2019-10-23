66e12cfdb255ff74e9f3396ae2e49c3a31ac2a83
arjuns <arjuns@SDD-1>
2019-10-23 11:30:55 +0530

    minor chnage

--- HumanitarianAssistance.Application/Project/Commands/Create/AddProjectHiringRequestCommandHandler.cs
+++ HumanitarianAssistance.Application/Project/Commands/Create/AddProjectHiringRequestCommandHandler.cs
@@ -28,16 +28,10 @@ namespace HumanitarianAssistance.Application.Project.Commands.Create
             {
                 ProjectHiringRequestDetail hiringRequestDeatil = new ProjectHiringRequestDetail()
                 {                     
-                   // BasicPay = request.BasicPay,
-                   // BudgetLineId = request.BudgetLineId,
                     CreatedById = request.CreatedById,
                     CreatedDate =request.CreatedDate,
-                   // Currency = request.Currency,
-                 //   Description = request.Description,      
-                 //   GradeId = request.GradeId,
                     IsDeleted = false,
                     OfficeId = request.Office,
-                   // Position = request.Position,
                     ProfessionId = request.Position,
                     ProjectId = request.ProjectId,
                     TotalVacancies = request.TotalVacancy,
@@ -61,47 +55,13 @@ namespace HumanitarianAssistance.Application.Project.Commands.Create
                     Experience=request.Experience,
                     Organization=request.Organization                    
                 };
+                if(request.JobCategory!=null)
+                {
+                    
+                }
                 await _dbContext.ProjectHiringRequestDetail.AddAsync(hiringRequestDeatil);
                 await _dbContext.SaveChangesAsync();
-
-                if (hiringRequestDeatil.HiringRequestId != 0)
-                {
-                    string description = string.Empty;
-                    JobHiringDetails jobDetail = new JobHiringDetails();
-                    if (!string.IsNullOrEmpty(request.Description))
-                    {
-                        description = request.Description.ToLower().Trim();
-                        jobDetail = await _dbContext.JobHiringDetails.FirstOrDefaultAsync(x => x.IsDeleted == false &&
-                                                                                          x.JobDescription.ToLower().Trim() == description);
-                    }
-                    if (jobDetail == null)
-                    {
-                        jobDetail = new JobHiringDetails()
-                        {
-                            JobDescription = request.Description,
-                            ProfessionId = request.ProfessionId,
-                            OfficeId = request.OfficeId,
-                            IsActive = true,
-                            GradeId = request.GradeId,
-                            HiringRequestId = hiringRequestDeatil.HiringRequestId,
-                            IsDeleted = false,
-                            CreatedById = request.CreatedById,
-                            CreatedDate = DateTime.UtcNow,
-                            Unit = request.TotalVacancies.Value
-                        };
-                        await _dbContext.JobHiringDetails.AddAsync(jobDetail);
-                        await _dbContext.SaveChangesAsync();
-                        if (jobDetail.JobId != 0)
-                        {
-                            jobDetail.JobCode = "JC" + String.Format("{0:D4}", jobDetail.JobId);
-                            await _dbContext.SaveChangesAsync();
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Job is already exist");
-                    }
-                }      
+    
                 response.StatusCode = StaticResource.successStatusCode;
                 response.Message = "Success";
             }
