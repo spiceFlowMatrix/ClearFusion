40d904512338eed363eded2001d968434beabd81
ramanr <ramanrawat03@gmail.com>
2019-11-11 14:56:48 +0530

    changes for store logs

--- HumanitarianAssistance.Application/Store/Commands/Create/AddVehicleMileageCommandHandler.cs
+++ HumanitarianAssistance.Application/Store/Commands/Create/AddVehicleMileageCommandHandler.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HumanitarianAssistance.Application.Store.Commands.Create
 {
-    public class AddVehicleMileageCommandHandler: IRequestHandler<AddVehicleMileageCommand, bool>
+    public class AddVehicleMileageCommandHandler : IRequestHandler<AddVehicleMileageCommand, bool>
     {
         private HumanitarianAssistanceDbContext _dbContext;
         public AddVehicleMileageCommandHandler(HumanitarianAssistanceDbContext dbContext)
@@ -19,52 +19,76 @@ namespace HumanitarianAssistance.Application.Store.Commands.Create
 
         public async Task<bool> Handle(AddVehicleMileageCommand request, CancellationToken cancellationToken)
         {
-            bool success= false;
+            bool success = false;
 
             try
             {
-                if(request != null) 
+                if (request != null)
                 {
 
-                    VehicleMileageDetail mileage = await _dbContext.VehicleMileageDetail.FirstOrDefaultAsync(x=> x.IsDeleted == false &&
+                    VehicleMileageDetail mileage = await _dbContext.VehicleMileageDetail.FirstOrDefaultAsync(x => x.IsDeleted == false &&
                                                                                         x.VehicleId == request.VehicleId &&
                                                                                         x.MileageMonth.Month == request.Month.Month);
 
-                    if(mileage == null)
+                    if (mileage == null)
                     {
-                         mileage= new VehicleMileageDetail
+                        mileage = new VehicleMileageDetail
                         {
-                            IsDeleted= false,
-                            CreatedDate= DateTime.UtcNow,
-                            CreatedById= request.CreatedById,
-                            VehicleId= request.VehicleId,
+                            IsDeleted = false,
+                            CreatedDate = DateTime.UtcNow,
+                            CreatedById = request.CreatedById,
+                            VehicleId = request.VehicleId,
                             MileageMonth = request.Month,
-                            Mileage= request.Mileage
+                            Mileage = request.Mileage
                         };
 
                         await _dbContext.VehicleMileageDetail.AddAsync(mileage);
+
+                        //log details
+                        StoreLogger logger = new StoreLogger
+                        {
+                            CreatedDate = DateTime.UtcNow,
+                            CreatedById = request.CreatedById,
+                            IsDeleted = false,
+                            EventType = "Mileage Added",
+                            LogText = $"{request.Mileage} Mileage added to Vehicle Id {request.VehicleId}"
+                        };
+
+                        await _dbContext.StoreLogger.AddAsync(logger);
                         await _dbContext.SaveChangesAsync();
                     }
-                    else 
+                    else
                     {
                         mileage.ModifiedDate = DateTime.UtcNow;
                         mileage.ModifiedById = request.CreatedById;
-                        mileage.VehicleId= request.VehicleId;
+                        mileage.VehicleId = request.VehicleId;
                         mileage.MileageMonth = request.Month;
-                        mileage.Mileage= request.Mileage;
+                        mileage.Mileage = request.Mileage;
 
                         _dbContext.VehicleMileageDetail.Update(mileage);
+
+                        //log details
+                        StoreLogger logger = new StoreLogger
+                        {
+                            CreatedDate = DateTime.UtcNow,
+                            CreatedById = request.CreatedById,
+                            IsDeleted = false,
+                            EventType = "Mileage Updated",
+                            LogText = $"{request.Mileage} Mileage added to Vehcile Id {request.VehicleId}"
+                        };
+
+                        await _dbContext.StoreLogger.AddAsync(logger);
                         await _dbContext.SaveChangesAsync();
                     }
 
-                   success= true;
+                    success = true;
                 }
                 else
                 {
                     throw new Exception(StaticResource.RequestValuesInAppropriate);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw ex;
             }
