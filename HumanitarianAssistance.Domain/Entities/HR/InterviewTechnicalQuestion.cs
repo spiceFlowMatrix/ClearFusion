a3eaca035ad8f30fb2c638c5d064c46356d8996d
arjuns <arjuns@SDD-1>
2019-11-22 17:36:16 +0530

    migration updated

--- HumanitarianAssistance.Domain/Entities/HR/InterviewTechnicalQuestion.cs
+++ HumanitarianAssistance.Domain/Entities/HR/InterviewTechnicalQuestion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using HumanitarianAssistance.Domain.Entities.Project;
 
 namespace HumanitarianAssistance.Domain.Entities.HR
 {
@@ -16,13 +17,10 @@ namespace HumanitarianAssistance.Domain.Entities.HR
 		public InterviewDetails InterviewDetails { get; set; }
 		public int? InterviewDetailsId { get; set; }
 		public string Question { get; set; }
-		//public TechnicalQuestion TechnicalQuestion { get; set; }
-		//public int Poor { get; set; }
-		//public int Fair { get; set; }
-		//public int Good { get; set; }
-		//public int Excellent { get; set; }
-		//public int Perfect { get; set; }
 		public string Answer { get; set; }
+		[ForeignKey ("InterviewId")]
+		public ProjectInterviewDetails ProjectInterviewDetails { get; set; }
+		public int? InterviewId { get; set; }
  		
 		[ForeignKey("QuestionId")]
 		public TechnicalQuestion TechnicalQuestion { get; set; }
