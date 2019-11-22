ada8560115bb832228efa062def976650f8bf015
arjuns <arjuns@SDD-1>
2019-11-22 12:49:38 +0530

    minor chnages

--- HumanitarianAssistance.Application/Project/Commands/Create/AddInterviewDetailsCommand.cs
+++ HumanitarianAssistance.Application/Project/Commands/Create/AddInterviewDetailsCommand.cs
@@ -13,15 +13,18 @@ namespace HumanitarianAssistance.Application.Project.Commands.Create
         public List<LanguageDetailsModel> LanguageList { get; set; }
         public List<TraningDetailsModel> TraningList { get; set; }
         public List<InterviewerDetailsModel> InterviewerList { get; set; }
+
+        public long CandidateId { get; set; }
+        public long HiringRequestId { get; set; }
         public string Description { get; set; }
-        public int? NoticePeriod { get; set; }
-        public DateTime? AvailableDate { get; set; }
-        public int? WrittenTestMarks { get; set; }
-        public int? CurrentBase { get; set; }
-        public int? CurrentOther { get; set; }
-        public int? ExpectationBase { get; set; }
-        public int? ExpectationOther { get; set; }
-        public int? Status { get; set; }
+        public int NoticePeriod { get; set; }
+        public DateTime AvailableDate { get; set; }
+        public int WrittenTestMarks { get; set; }
+        public int CurrentBase { get; set; }
+        public int CurrentOther { get; set; }
+        public int ExpectationBase { get; set; }
+        public int ExpectationOther { get; set; }
+        public int Status { get; set; }
         public bool InterviewQuestionOne { get; set; }
         public bool InterviewQuestionTwo { get; set; }
         public bool InterviewQuestionThree { get; set; }
@@ -29,8 +32,8 @@ namespace HumanitarianAssistance.Application.Project.Commands.Create
         public bool CurrentMeal { get; set; }
         public bool ExpectationTransport { get; set; }
         public bool ExpectationMeal { get; set; }
-        public double? ProfessionalCriteriaMark { get; set; }
-        public int? MarksObtain { get; set; }
-        public int? TotalMarksObtain { get; set; }
+        public double ProfessionalCriteriaMark { get; set; }
+        public int MarksObtain { get; set; }
+        public int TotalMarksObtain { get; set; }
     }
 }
--- HumanitarianAssistance.Application/Project/Commands/Create/AddInterviewDetailsCommandHandler.cs
+++ HumanitarianAssistance.Application/Project/Commands/Create/AddInterviewDetailsCommandHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +7,8 @@ using AutoMapper;
 using HumanitarianAssistance.Application.Infrastructure;
 using HumanitarianAssistance.Common.Enums;
 using HumanitarianAssistance.Common.Helpers;
+using HumanitarianAssistance.Domain.Entities;
+using HumanitarianAssistance.Domain.Entities.HR;
 using HumanitarianAssistance.Domain.Entities.Project;
 using HumanitarianAssistance.Persistence;
 using MediatR;
@@ -21,6 +24,115 @@ namespace HumanitarianAssistance.Application.Project.Commands.Create {
         public async Task<ApiResponse> Handle (AddInterviewDetailsCommand request, CancellationToken cancellationToken) {
             ApiResponse response = new ApiResponse ();
             try {
+                ProjectInterviewDetails interviewDetails = new ProjectInterviewDetails () {
+                    Description = request.Description,
+                    NoticePeriod = request.NoticePeriod,
+                    AvailableDate = request.AvailableDate,
+                    WrittenTestMarks = request.WrittenTestMarks,
+                    CurrentBase = request.CurrentBase,
+                    CurrentOther = request.CurrentOther,
+                    ExpectationBase = request.ExpectationBase,
+                    ExpectationOther = request.ExpectationOther,
+                    Status = request.Status,
+                    InterviewQuestionOne = request.InterviewQuestionOne,
+                    InterviewQuestionTwo = request.InterviewQuestionTwo,
+                    InterviewQuestionThree = request.InterviewQuestionThree,
+                    CurrentTransport = request.CurrentTransport,
+                    CurrentMeal = request.CurrentMeal,
+                    ExpectationTransport = request.ExpectationTransport,
+                    ExpectationMeal = request.ExpectationMeal,
+                    ProfessionalCriteriaMarks = request.ProfessionalCriteriaMark,
+                    MarksObtained = request.MarksObtain,
+                    // TotalMarksObtained =request.TotalMarksObtain,
+                    CreatedById = request.CreatedById,
+                    CreatedDate = request.CreatedDate,
+                    IsDeleted = false
+                };
+                await _dbContext.ProjectInterviewDetails.AddAsync (interviewDetails);
+                await _dbContext.SaveChangesAsync ();
+                List<RatingBasedCriteria> ratingObj = new List<RatingBasedCriteria> ();
+                foreach (var item in request.RatingBasedCriteriaList) {
+                    RatingBasedCriteria question = new RatingBasedCriteria () {
+                        QuestionId = item.QuestionId,
+                        Score = item.Score,
+                        CreatedById = request.CreatedById,
+                        CreatedDate = request.CreatedDate,
+                        IsDeleted = false
+                    };
+
+                    ratingObj.Add (question);
+                }
+                await _dbContext.RatingBasedCriteria.AddRangeAsync (ratingObj);
+                await _dbContext.SaveChangesAsync ();
+
+                List<InterviewTechnicalQuestion> technicalObj = new List<InterviewTechnicalQuestion> ();
+                foreach (var item in request.TechnicalQuestionList) {
+                    InterviewTechnicalQuestion question = new InterviewTechnicalQuestion () {
+                        QuestionId = item.QuestionId,
+                        Score = item.Score,
+                        CreatedById = request.CreatedById,
+                        CreatedDate = request.CreatedDate,
+                        IsDeleted = false
+                    };
+
+                    technicalObj.Add (question);
+                }
+                await _dbContext.InterviewTechnicalQuestion.AddRangeAsync (technicalObj);
+                await _dbContext.SaveChangesAsync ();
+
+                List<InterviewLanguages> languageObj = new List<InterviewLanguages> ();
+                foreach (var item in request.LanguageList) {
+                    InterviewLanguages details = new InterviewLanguages () {
+                        LanguageName = item.LanguageName,
+                        Reading = 1,
+                        Writing = 1,
+                        Listening = 1,
+                        Speaking = 1,
+                        CreatedById = request.CreatedById,
+                        CreatedDate = request.CreatedDate,
+                        IsDeleted = false
+                    };
+
+                    languageObj.Add (details);
+                }
+                await _dbContext.InterviewLanguages.AddRangeAsync (languageObj);
+                await _dbContext.SaveChangesAsync ();
+
+                List<InterviewTrainings> traningObj = new List<InterviewTrainings> ();
+                foreach (var item in request.TraningList) {
+                    InterviewTrainings details = new InterviewTrainings () {
+                        NewTraininigType = item.TraningType,
+                        TrainingName = item.TraningName,
+                        StudyingCountry = item.TraningCountryAndCity,
+                        StartDate = item.TraningStartDate,
+                        EndDate = item.TraningEndDate,
+                        CreatedById = request.CreatedById,
+                        CreatedDate = request.CreatedDate,
+                        IsDeleted = false
+                    };
+                    traningObj.Add (details);
+                }
+                await _dbContext.InterviewTrainings.AddRangeAsync (traningObj);
+                await _dbContext.SaveChangesAsync ();
+
+                List<HRJobInterviewers> interviewerObj = new List<HRJobInterviewers> ();
+                foreach (var item in request.InterviewerList) {
+                    HRJobInterviewers details = new HRJobInterviewers () {
+                        EmployeeId = item.EmployeeId,
+                        CreatedById = request.CreatedById,
+                        CreatedDate = request.CreatedDate,
+                        IsDeleted = false
+                    };
+                    interviewerObj.Add (details);
+                }
+                await _dbContext.HRJobInterviewers.AddRangeAsync (interviewerObj);
+                await _dbContext.SaveChangesAsync ();
+
+                var statusDetails = _dbContext.HiringRequestCandidateStatus.Where (x => x.HiringRequestId == request.HiringRequestId && x.CandidateId == request.CandidateId && x.IsDeleted == false).FirstOrDefault ();
+                statusDetails.ModifiedById = request.CreatedById;
+                statusDetails.ModifiedDate = request.CreatedDate;
+                statusDetails.InterviewId = interviewDetails.InterviewId;
+                await _dbContext.SaveChangesAsync ();
                 response.StatusCode = StaticResource.successStatusCode;
                 response.Message = StaticResource.SuccessText;
             } catch (Exception ex) {
