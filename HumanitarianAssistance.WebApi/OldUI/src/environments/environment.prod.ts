eeb46879e26c456e752480eae0110ea937c53230
poonamk <poonamk@SDD-29>
2019-11-27 12:17:23 +0530

    minor changes

--- HumanitarianAssistance.WebApi/OldUI/src/environments/environment.prod.ts
+++ HumanitarianAssistance.WebApi/OldUI/src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
 export const environment = {
-  production: true,
-  apiUrl: '/api/',
-  docUrl: '/Docs/',
-  hubUrl: '/chathub/'
+  production: false,
+    apiUrl: '/api/',
+    docUrl: '/Docs/',
+    hubUrl: '/chathub/'
 };
+
+// export const environment = {
+//   production: false,
+//     apiUrl: 'http://localhost:5000/api/',
+//     docUrl: 'http://localhost:5000/Docs/',
+//     hubUrl: 'http://localhost:5000/notifyhub/'
+// };
