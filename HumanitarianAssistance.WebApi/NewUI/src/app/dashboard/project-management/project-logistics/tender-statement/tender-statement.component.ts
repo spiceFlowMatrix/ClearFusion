c24b444f4c7902b4873cda883c4905c702dfa122
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2019-11-27 10:08:04 +0530

    tender component added

---
+++ HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/project-management/project-logistics/tender-statement/tender-statement.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tender-statement',
+  templateUrl: './tender-statement.component.html',
+  styleUrls: ['./tender-statement.component.scss']
+})
+export class TenderStatementComponent implements OnInit {
+
+  @Input() requestStatus = 0;
+  @Input() totalCost = 0;
+  @Input() tenderStatus = 3;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
