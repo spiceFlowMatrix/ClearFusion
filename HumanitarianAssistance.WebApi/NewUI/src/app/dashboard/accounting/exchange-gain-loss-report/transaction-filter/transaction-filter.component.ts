defcdfd6b4fa1fdf8f70039579256508c2fc8e5b
ramanr <ramanrawat03@gmail.com>
2019-12-27 10:33:11 +0530

    changes for exchange gain loss

---
+++ HumanitarianAssistance.WebApi/NewUI/src/app/dashboard/accounting/exchange-gain-loss-report/transaction-filter/transaction-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-filter',
+  templateUrl: './transaction-filter.component.html',
+  styleUrls: ['./transaction-filter.component.scss']
+})
+export class TransactionFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
