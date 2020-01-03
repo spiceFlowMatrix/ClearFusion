9540aa1a7db2f92a0881ba714a43890625755048
ramanr <ramanrawat03@gmail.com>
2020-01-03 14:33:31 +0530

    minor change

---
+++ HumanitarianAssistance.Domain/Entities/Accounting/ConsolidatedGainLossAccounts.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HumanitarianAssistance.Domain.Entities.Accounting
+{
+    public class ConsolidatedGainLossAccounts
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(Order = 1)]
+        public long Id { get; set; }
+        public long[] AccountIds { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
