d077d9f2b18cf4df428389bec9f63c529d2f59dc
avinashr <avinashr@DESKTOP-6OB5DTP>
2020-01-03 13:37:55 +0530

    migrations changes

---
+++ HumanitarianAssistance.Persistence/Migrations/20191210072113_storeAndLogisticTblUpdated.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanitarianAssistance.Persistence.Migrations
+{
+    public partial class storeAndLogisticTblUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                table: "PurchaseUnitType",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChasisNo",
+                table: "PurchasedVehicleDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EngineNo",
+                table: "PurchasedVehicleDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManufacturerCountry",
+                table: "PurchasedVehicleDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonRemarks",
+                table: "PurchasedVehicleDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationNo",
+                table: "PurchasedVehicleDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChasisNo",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeID",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EngineNo",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManufacturerCountry",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonRemarks",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationNo",
+                table: "PurchasedGeneratorDetail",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "ProjectLogisticItems",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "CandidateDetails",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchasedGeneratorDetail_EmployeeID",
+                table: "PurchasedGeneratorDetail",
+                column: "EmployeeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchasedGeneratorDetail_EmployeeDetail_EmployeeID",
+                table: "PurchasedGeneratorDetail",
+                column: "EmployeeID",
+                principalTable: "EmployeeDetail",
+                principalColumn: "EmployeeID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchasedGeneratorDetail_EmployeeDetail_EmployeeID",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchasedGeneratorDetail_EmployeeID",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                table: "PurchaseUnitType");
+
+            migrationBuilder.DropColumn(
+                name: "ChasisNo",
+                table: "PurchasedVehicleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "EngineNo",
+                table: "PurchasedVehicleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturerCountry",
+                table: "PurchasedVehicleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "PersonRemarks",
+                table: "PurchasedVehicleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationNo",
+                table: "PurchasedVehicleDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ChasisNo",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeID",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "EngineNo",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturerCountry",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "PersonRemarks",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationNo",
+                table: "PurchasedGeneratorDetail");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "CandidateDetails");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Quantity",
+                table: "ProjectLogisticItems",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
---
+++ HumanitarianAssistance.Persistence/Migrations/20191213064545_UpdatedLogisticsSupplierTbl.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanitarianAssistance.Persistence.Migrations
+{
+    public partial class UpdatedLogisticsSupplierTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupplierName",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.RenameColumn(
+                name: "FinalPrice",
+                table: "ProjectLogisticSuppliers",
+                newName: "FinalUnitPrice");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ItemId",
+                table: "ProjectLogisticSuppliers",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StoreSourceCodeId",
+                table: "ProjectLogisticSuppliers",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectLogisticSuppliers_ItemId",
+                table: "ProjectLogisticSuppliers",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectLogisticSuppliers_StoreSourceCodeId",
+                table: "ProjectLogisticSuppliers",
+                column: "StoreSourceCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectLogisticSuppliers_InventoryItems_ItemId",
+                table: "ProjectLogisticSuppliers",
+                column: "ItemId",
+                principalTable: "InventoryItems",
+                principalColumn: "ItemId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectLogisticSuppliers_StoreSourceCodeDetail_StoreSourceC~",
+                table: "ProjectLogisticSuppliers",
+                column: "StoreSourceCodeId",
+                principalTable: "StoreSourceCodeDetail",
+                principalColumn: "SourceCodeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectLogisticSuppliers_InventoryItems_ItemId",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectLogisticSuppliers_StoreSourceCodeDetail_StoreSourceC~",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectLogisticSuppliers_ItemId",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectLogisticSuppliers_StoreSourceCodeId",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.DropColumn(
+                name: "StoreSourceCodeId",
+                table: "ProjectLogisticSuppliers");
+
+            migrationBuilder.RenameColumn(
+                name: "FinalUnitPrice",
+                table: "ProjectLogisticSuppliers",
+                newName: "FinalPrice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierName",
+                table: "ProjectLogisticSuppliers",
+                nullable: true);
+        }
+    }
+}
