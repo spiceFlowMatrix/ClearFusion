82fa87f8a37f5657bc71c6b56170af45b8994f7b
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2020-01-04 12:27:08 +0530

    added col to tenderBidSubmission

---
+++ HumanitarianAssistance.Persistence/Migrations/20200104065552_AddedColToTenderBidSubmission.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanitarianAssistance.Persistence.Migrations
+{
+    public partial class AddedColToTenderBidSubmission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBidSubmitted",
+                table: "TenderBidSubmission",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBidSubmitted",
+                table: "TenderBidSubmission");
+        }
+    }
+}
