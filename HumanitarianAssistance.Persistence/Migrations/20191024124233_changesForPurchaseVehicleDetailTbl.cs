aa94057bc425d74c0f2adf9e9193b8f42f8fc86c
avinashr <avinashr@DESKTOP-6OB5DTP>
2020-01-07 17:15:06 +0530

    Migrations updated

---
+++ HumanitarianAssistance.Persistence/Migrations/20191024124233_changesForPurchaseVehicleDetailTbl.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanitarianAssistance.Persistence.Migrations
+{
+    public partial class changesForPurchaseVehicleDetailTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleItemDetail_PurchaseId",
+                table: "VehicleItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleItemDetail_VehiclePurchaseId",
+                table: "VehicleItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GeneratorItemDetail_GeneratorPurchaseId",
+                table: "GeneratorItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GeneratorItemDetail_PurchaseId",
+                table: "GeneratorItemDetail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleItemDetail_PurchaseId",
+                table: "VehicleItemDetail",
+                column: "PurchaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleItemDetail_VehiclePurchaseId",
+                table: "VehicleItemDetail",
+                column: "VehiclePurchaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneratorItemDetail_GeneratorPurchaseId",
+                table: "GeneratorItemDetail",
+                column: "GeneratorPurchaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneratorItemDetail_PurchaseId",
+                table: "GeneratorItemDetail",
+                column: "PurchaseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleItemDetail_PurchaseId",
+                table: "VehicleItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleItemDetail_VehiclePurchaseId",
+                table: "VehicleItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GeneratorItemDetail_GeneratorPurchaseId",
+                table: "GeneratorItemDetail");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GeneratorItemDetail_PurchaseId",
+                table: "GeneratorItemDetail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleItemDetail_PurchaseId",
+                table: "VehicleItemDetail",
+                column: "PurchaseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleItemDetail_VehiclePurchaseId",
+                table: "VehicleItemDetail",
+                column: "VehiclePurchaseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneratorItemDetail_GeneratorPurchaseId",
+                table: "GeneratorItemDetail",
+                column: "GeneratorPurchaseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GeneratorItemDetail_PurchaseId",
+                table: "GeneratorItemDetail",
+                column: "PurchaseId",
+                unique: true);
+        }
+    }
+}
