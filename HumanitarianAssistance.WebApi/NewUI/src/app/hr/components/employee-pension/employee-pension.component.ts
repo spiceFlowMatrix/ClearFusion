a979e99cef16b37ad95fc34bcb460b1653a5e399
poonamk <poonamk@SDD-29>
2020-01-17 15:08:40 +0530

    changes  for employee pension detail

--- HumanitarianAssistance.WebApi/NewUI/src/app/hr/components/employee-pension/employee-pension.component.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/hr/components/employee-pension/employee-pension.component.ts
@@ -3,7 +3,9 @@ import { IDropDownModel } from './../../../store/models/purchase';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CommonLoaderService } from 'src/app/shared/common-loader/common-loader.service';
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin, ReplaySubject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-employee-pension',
@@ -11,6 +13,7 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./employee-pension.component.scss']
 })
 export class EmployeePensionComponent implements OnInit {
+  // dataSource
   pensionListHeaders$ = of([
     'Date',
     'Gross Salary',
@@ -20,6 +23,8 @@ export class EmployeePensionComponent implements OnInit {
     'Total'
   ]);
   taxListHeaders$ = of(['Date', 'Currency', 'Office', 'Total Tax']);
+
+  // variables
   pensionList$: Observable<any[]>;
   taxList$: Observable<any[]>;
   selectedCurrencyTax: IDropDownModel;
@@ -28,69 +33,169 @@ export class EmployeePensionComponent implements OnInit {
   selectedFinancialYearTax: IDropDownModel;
   currencyList$: Observable<IDropDownModel[]>;
   financialYearList$: Observable<IDropDownModel[]>;
+  employeeId: number;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
   constructor(
     private toastr: ToastrService,
     private commonLoader: CommonLoaderService,
-    private employeePensionService: EmployeePensionService
-  ) {
-    this.selectedCurrencyTax = { value: 0, name: 'Display Currency' };
-    this.selectedCurrencyPension = { value: 0, name: 'Display Currency' };
-    this.selectedFinancialYearTax = { value: 0, name: 'Financial Year' };
-    this.selectedFinancialYearPension = { value: 0, name: 'Financial Year' };
-  }
+    private employeePensionService: EmployeePensionService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getCurrencyList();
     this.getFinancialYearList();
+    this.activeRoute.params.subscribe(params => {
+      this.employeeId = params['id'];
+    });
+
+    forkJoin([this.getCurrencyList(), this.getFinancialYearList()])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(result => {
+        this.subscribeAllCurrency(result[0]);
+        this.subscribeAllFinanacialYear(result[1]);
+      });
+    // this.getAllPensionList(data.FinancialYearId, data.CurrencyId);
+    // this.getAllSalaryTaxData(data.FinancialYearId, data.CurrencyId);
+    // this.getEmployeeTaxCalculation(this.employeeId, data.FinancialYearId);
   }
   //#region "get currency  List"
   getCurrencyList() {
-    this.commonLoader.showLoader();
-    this.employeePensionService.GetCurrencyList().subscribe(
-      x => {
-        this.commonLoader.hideLoader();
-        if (x.data.CurrencyList.length > 0) {
-          this.currencyList$ = of(
-            x.data.CurrencyList.map(y => {
-              return {
-                value: y.CurrencyId,
-                name: y.CurrencyName
-              } as IDropDownModel;
-            })
-          );
-        }
-      },
-      () => {
-        this.commonLoader.hideLoader();
-      }
-    );
+    return this.employeePensionService.GetCurrencyList();
+    // debugger;
+    // this.commonLoader.showLoader();
+    // this.employeePensionService.GetCurrencyList().subscribe(
+    //   x => {
+    //     this.commonLoader.hideLoader();
+    //     if (x.data.CurrencyList.length > 0) {
+    //       this.currencyList$ = of(
+    //         x.data.CurrencyList.map(y => {
+    //           return {
+    //             value: y.CurrencyId,
+    //             name: y.CurrencyName
+    //           } as IDropDownModel;
+    //         })
+    //       );
+    //       this.currencyList$.subscribe(element => {
+    //         // pre selected currency
+    //         this.selectedCurrencyTax = {
+    //           value: element[0].value,
+    //           name: element[0].name
+    //         };
+    //         this.selectedCurrencyPension = {
+    //           value: element[0].value,
+    //           name: element[0].name
+    //         };
+    //       });
+    //     }
+    //   },
+    //   () => {
+    //     this.commonLoader.hideLoader();
+    //   }
+    // );
   }
   //#endregion
+  subscribeAllCurrency(response: any) {
+    this.currencyList$ = of(
+      response.data.CurrencyList.map(y => {
+        return {
+          value: y.CurrencyId,
+          name: y.CurrencyName
+        } as IDropDownModel;
+      })
+    );
+    this.currencyList$.subscribe(element => {
+      // pre selected currency
+      this.selectedCurrencyTax = {
+        value: element[0].value,
+        name: element[0].name
+      };
+      this.selectedCurrencyPension = {
+        value: element[0].value,
+        name: element[0].name
+      };
+    });
+  }
 
   //#region "get Financial Year List"
   getFinancialYearList() {
     this.commonLoader.showLoader();
-    this.employeePensionService.GetFinancialYearList().subscribe(
-      x => {
-        this.commonLoader.hideLoader();
-        if (x.data.FinancialYearDetailList.length > 0) {
-          this.financialYearList$ = of(
-            x.data.FinancialYearDetailList.map(y => {
-              return {
-                value: y.FinancialYearId,
-                name: y.FinancialYearName
-              } as IDropDownModel;
-            })
-          );
-        }
-      },
-      () => {
-        this.commonLoader.hideLoader();
-      }
-    );
+    return this.employeePensionService.GetFinancialYearList();
+    // this.employeePensionService.GetFinancialYearList().subscribe(
+    //   x => {
+    //     this.commonLoader.hideLoader();
+    //     if (
+    //       x.data.FinancialYearDetailList !== undefined &&
+    //       x.data.FinancialYearDetailList.length > 0
+    //     ) {
+    //       this.financialYearList$ = of(
+    //         x.data.FinancialYearDetailList.map(y => {
+    //           return {
+    //             value: y.FinancialYearId,
+    //             name: y.FinancialYearName
+    //           } as IDropDownModel;
+    //         })
+    //       );
+    //       this.financialYearList$.subscribe(element => {
+    //         this.selectedFinancialYearTax = {
+    //           value: element[0].value,
+    //           name: element[0].name
+    //         };
+    //         this.selectedFinancialYearPension = {
+    //           value: element[0].value,
+    //           name: element[0].name
+    //         };
+    //       });
+    //     } else {
+    //       this.selectedFinancialYearPension = {
+    //         value: 0,
+    //         name: "Financial Year"
+    //       };
+    //       this.selectedFinancialYearTax = { value: 0, name: "Financial Year" };
+    //     }
+    //   },
+    //   error => {
+    //     this.commonLoader.hideLoader();
+    //   }
+    // );
   }
   //#endregion
 
+  subscribeAllFinanacialYear(response: any) {
+    if (
+      response.data.FinancialYearDetailList !== undefined &&
+      response.data.FinancialYearDetailList.length > 0
+    ) {
+      this.financialYearList$ = of(
+        response.data.FinancialYearDetailList.map(y => {
+          return {
+            value: y.FinancialYearId,
+            name: y.FinancialYearName
+          } as IDropDownModel;
+        })
+      );
+      this.commonLoader.hideLoader();
+      this.financialYearList$.subscribe(element => {
+        this.selectedFinancialYearTax = {
+          value: element[0].value,
+          name: element[0].name
+        };
+        this.selectedFinancialYearPension = {
+          value: element[0].value,
+          name: element[0].name
+        };
+      }, error => {
+        this.commonLoader.hideLoader();
+      });
+    } else {
+      this.selectedFinancialYearPension = {
+        value: 0,
+        name: 'Financial Year'
+      };
+      this.selectedFinancialYearTax = { value: 0, name: 'Financial Year' };
+    }
+  }
   selectedCurrencyChangedForTax(taxCurrency: any) {
     this.selectedCurrencyTax = {
       value: taxCurrency.value,
@@ -115,4 +220,15 @@ export class EmployeePensionComponent implements OnInit {
       name: financialYear.name
     };
   }
+
+  //#region "getAllPensionList"
+  getAllPensionList(yearId: number[], currencyId: number) {
+    const model = {
+      OfficeId: parseInt(localStorage.getItem('EMPLOYEEOFFICEID'), 32),
+      EmployeeId: this.employeeId,
+      FinancialYearId: yearId,
+      CurrencyId: currencyId
+    };
+  }
+  //#endregion
 }
