b197aa29e9ecf5d08a04f86023c0404cfc18f4bb
ramanr <ramanrawat03@gmail.com>
2020-01-27 13:50:50 +0530

    changes for advancesTbl

--- HumanitarianAssistance.Domain/Entities/HR/Advances.cs
+++ HumanitarianAssistance.Domain/Entities/HR/Advances.cs
@@ -18,22 +18,22 @@ namespace HumanitarianAssistance.Domain.Entities.HR
         [ForeignKey("EmployeeId")]
         public EmployeeDetail EmployeeDetail { get; set; }
         public string EmployeeCode { get; set; }
-        public int CurrencyId { get; set; }
+        public int? CurrencyId { get; set; }
         [ForeignKey("CurrencyId")]
         public CurrencyDetails CurrencyDetails { get; set; }
         public long? VoucherReferenceNo { get; set; }
         public string Description { get; set; }
         public string ModeOfReturn { get; set; }
-        public int ApprovedBy { get; set; }
+        public int? ApprovedBy { get; set; }
         public double RequestAmount { get; set; }
         public double AdvanceAmount { get; set; }
-        public int OfficeId { get; set; }
-        public bool IsApproved { get; set; }        // Is advance approved by manager or not (false means not approved)
+        public int? OfficeId { get; set; }
+        public bool? IsApproved { get; set; }  
         public bool IsDeducted { get; set; }        // Is advance amount deducted from next month salary when defining monthly salary payroll (false means not distributed)
         public DateTime AppraisalApprovedDate { get; set; }
-        public DateTime DeductedDate { get; set; }
+        public DateTime? DeductedDate { get; set; }
         public bool IsAdvanceRecovery { get; set; }
-        public DateTime AdvanceRecoveryDate { get; set; }
+        public DateTime? AdvanceRecoveryDate { get; set; }
         public int? NumberOfInstallments { get; set; }
         [ForeignKey("ApprovedBy")]
         public EmployeeDetail ApprovedByEmployee { get; set; }
