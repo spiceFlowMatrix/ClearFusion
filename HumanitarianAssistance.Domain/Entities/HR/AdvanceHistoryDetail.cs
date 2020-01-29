2a6d7873b2101792d190f61517adbc6bb89fef24
ramanr <ramanrawat03@gmail.com>
2020-01-29 10:04:37 +0530

    changes for AdvanceHistoryDetail table

--- HumanitarianAssistance.Domain/Entities/HR/AdvanceHistoryDetail.cs
+++ HumanitarianAssistance.Domain/Entities/HR/AdvanceHistoryDetail.cs
@@ -14,7 +14,10 @@ namespace HumanitarianAssistance.Domain.Entities.HR
         public DateTime PaymentDate { get; set; }
         public double InstallmentPaid { get; set; }
         public double InstallmentBalance { get; set; }
+        public int EmployeeId { get; set; }
         [ForeignKey("AdvanceId")]
         public Advances Advances { get; set; }
+        [ForeignKey("EmployeeId")]
+        public EmployeeDetail EmployeeDetail { get; set; }
     }
 }
