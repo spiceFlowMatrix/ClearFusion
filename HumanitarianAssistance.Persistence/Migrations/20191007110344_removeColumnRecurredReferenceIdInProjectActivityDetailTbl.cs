a1a11e696f90e42f77d8a894b47d8a17369d3ef4
avinashr <avinashr@DESKTOP-6OB5DTP>
2020-01-31 12:49:09 +0530

    Migrations updates

---
+++ HumanitarianAssistance.Persistence/Migrations/20191007110344_removeColumnRecurredReferenceIdInProjectActivityDetailTbl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanitarianAssistance.Persistence.Migrations
+{
+    public partial class removeColumnRecurredReferenceIdInProjectActivityDetailTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReoccuredReferenceId",
+                table: "ProjectActivityDetail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ReoccuredReferenceId",
+                table: "ProjectActivityDetail",
+                nullable: true);
+        }
+    }
+}
