785248e60e5c3f4577f18b40d8670ce363fa101f
Saksham Srivastava <Saksham Srivastava@DESKTOP-ETMF01D>
2020-02-11 20:02:45 +0530

    administer payroll changes

--- HumanitarianAssistance.WebApi/NewUI/src/app/shared/global.ts
+++ HumanitarianAssistance.WebApi/NewUI/src/app/shared/global.ts
@@ -211,6 +211,7 @@ export class GLOBAL {
   public static API_EmployeePayroll_GetEmployeeAdvanceHistoryDetail = 'EmployeePayroll/GetEmployeeAdvanceHistoryDetail';
   public static API_EmployeePayroll_AddAdvanceRecovery = 'EmployeePayroll/AddAdvanceRecovery';
   public static API_EmployeePayroll_GetEmployeePayrollAdvanceDetail = 'EmployeePayroll/GetEmployeePayrollAdvanceDetail';
+  public static API_HR_GetPayrollAdministrationDetailById = 'EmployeePayroll/GetPayrollAdministrationDetailById';
 
   public static API_EmployeeDetail_GetEmployeeDetailById = 'EmployeeDetail/GetEmployeeDetailById';
   public static API_HR_GetAllEmployeeAssignLeave = 'Attendance/GetAllEmployeeAssignLeave';
