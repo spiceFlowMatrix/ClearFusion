38d5beaeb824f2b9b732c3e5c10dd32420d4d41f
ramanr <ramanrawat03@gmail.com>
2020-02-27 14:30:11 +0530

    Issue Id   :#415 Message    :Migration add Transcript :Excel Export functionality for Employee Payroll

--- HumanitarianAssistance.Persistence/Migrations/20200227085747_AddColumnInEmployeePayrollInfoDetail.Designer.cs
+++ HumanitarianAssistance.Persistence/Migrations/20200227085747_AddColumnInEmployeePayrollInfoDetail.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using HumanitarianAssistance.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HumanitarianAssistance.Persistence.Migrations
 {
     [DbContext(typeof(HumanitarianAssistanceDbContext))]
-    partial class HumanitarianAssistanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200227085747_AddColumnInEmployeePayrollInfoDetail")]
+    partial class AddColumnInEmployeePayrollInfoDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
